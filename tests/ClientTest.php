eb56b7a152eeaf09300ba9f216e7c375d0f7c585
Vasil Dakov <vasildakov@gmail.com>
2016-02-23 11:58:37 -0600

    Small fix

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -11,40 +11,5 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 {
     public function testClientCanBeConstructed()
     {
-		/* $client = new Client();
-		$description = new Description([
-		    'baseUrl' => 'https://httpbin.org/',
-		    'operations' => [
-		        'someMethod' => [
-		            'httpMethod' => 'GET',
-		            'uri' => '/get?foo=bar',
-		            //'responseType' => 'class',
-					//'responseClass' => 'VasilDakov\\MaxOptra\\Response\\Test',
-		            'responseModel' => 'getResponse',
-		            'parameters' => [
-		                'foo' => [
-		                    'type' => 'string',
-		                    'location' => 'uri'
-		                ],
-		                'bar' => [
-		                    'type' => 'string',
-		                    'location' => 'query'
-		                ]
-		            ]
-		        ]
-		    ],
-		    'models' => [
-		        'getResponse' => [
-		            'type' => 'object',
-		            'additionalProperties' => [
-		                'location' => 'json'
-		            ]
-		        ]
-		    ]
-		]);
-
-		$guzzleClient = new GuzzleClient($client, $description);
-
-		$result = $guzzleClient->someMethod(['foo' => 'bar']); */
     }
 }
